56c3a37d65104828cf31d61e0bc1a51f93d20539
ronnnnn <xxxx.xyzs.xxxx@gmail.com>
2023-06-26 18:24:08 +0900

    test: add test for all properties of MediaQueryData

--- packages/nilts_test/test/lints/use_media_query_xxx_of.dart
+++ packages/nilts_test/test/lints/use_media_query_xxx_of.dart
@@ -24,6 +24,41 @@ class MainApp extends StatelessWidget {
     // expect_lint: use_media_query_xxx_of
     final maybeSize = MediaQuery.maybeOf(context)?.size;
 
+    // expect_lint: use_media_query_xxx_of
+    final orientation = MediaQuery.of(context).orientation;
+    // expect_lint: use_media_query_xxx_of
+    final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+    // expect_lint: use_media_query_xxx_of
+    final textScaleFactor = MediaQuery.of(context).textScaleFactor;
+    // expect_lint: use_media_query_xxx_of
+    final platformBrightness = MediaQuery.of(context).platformBrightness;
+    // expect_lint: use_media_query_xxx_of
+    final padding = MediaQuery.of(context).padding;
+    // expect_lint: use_media_query_xxx_of
+    final viewInsets = MediaQuery.of(context).viewInsets;
+    // expect_lint: use_media_query_xxx_of
+    final systemGestureInsets = MediaQuery.of(context).systemGestureInsets;
+    // expect_lint: use_media_query_xxx_of
+    final viewPadding = MediaQuery.of(context).viewPadding;
+    // expect_lint: use_media_query_xxx_of
+    final alwaysUse24HourFormat = MediaQuery.of(context).alwaysUse24HourFormat;
+    // expect_lint: use_media_query_xxx_of
+    final accessibleNavigation = MediaQuery.of(context).accessibleNavigation;
+    // expect_lint: use_media_query_xxx_of
+    final invertColors = MediaQuery.of(context).invertColors;
+    // expect_lint: use_media_query_xxx_of
+    final highContrast = MediaQuery.of(context).highContrast;
+    // expect_lint: use_media_query_xxx_of
+    final disableAnimations = MediaQuery.of(context).disableAnimations;
+    // expect_lint: use_media_query_xxx_of
+    final boldText = MediaQuery.of(context).boldText;
+    // expect_lint: use_media_query_xxx_of
+    final navigationMode = MediaQuery.of(context).navigationMode;
+    // expect_lint: use_media_query_xxx_of
+    final gestureSettings = MediaQuery.of(context).gestureSettings;
+    // expect_lint: use_media_query_xxx_of
+    final displayFeatures = MediaQuery.of(context).displayFeatures;
+
     return const MaterialApp(
       home: Scaffold(
         body: Center(
