0d179aab466cb398160611f4193e668eb88e04c7
ronnnnn <xxxx.xyzs.xxxx@gmail.com>
2023-06-26 18:19:14 +0900

    docs: add dart docs

--- packages/nilts/lib/src/change_priority.dart
+++ packages/nilts/lib/src/change_priority.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: comment_references
+
+/// A class for defining priorities of dart fix and assist in nilts.
 class ChangePriority {
+  /// The priority for [_ReplaceWithMediaQueryXxxOf].
   static const int replaceWithMediaQueryXxxOf = 100;
 }
--- packages/nilts/lib/src/lints/use_media_query_xxx_of.dart
+++ packages/nilts/lib/src/lints/use_media_query_xxx_of.dart
@@ -6,13 +6,41 @@ import 'package:analyzer/source/source_range.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:nilts/src/change_priority.dart';
 
+/// A class for `use_media_query_xxx_of` rule.
+///
+/// This rule checks if
+/// `MediaQuery.of(context)` or `MediaQuery.maybeOf(context)` is used
+/// instead of `MediaQuery.xxxOf(context)` or `MediaQuery.maybeXxxOf(context)`.
+///
+/// Prefer using
+/// 'MediaQuery.xxxOf(context) or MediaQuery.maybeXxxOf(context)
+/// instead of MediaQuery.of(context) or MediaQuery.maybeOf(context)
+/// to avoid unnecessary rebuilds.
+///
+/// **BAD:**
+/// ```dart
+/// final size = MediaQuery.of(context).size;
+/// ```
+///
+/// **GOOD:**
+/// ```dart
+/// final size = MediaQuery.sizeOf(context);
+/// ```
+///
+/// Note that using `MediaQuery.of(context)` or `MediaQuery.maybeOf(context)`
+/// makes sense in case of observing `MediaQueryData` object changes or
+/// referring to many properties of `MediaQueryData`.
+///
+/// See also: https://github.com/flutter/flutter/pull/114459
 class UseMediaQueryXxxOf extends DartLintRule {
+  /// Create a new instance of UseMediaQueryXxxOf.
   const UseMediaQueryXxxOf() : super(code: _code);
 
   static const _code = LintCode(
     name: 'use_media_query_xxx_of',
-    problemMessage: 'Prefer using MediaQuery.xxxOf(context) instead of '
-        'MediaQuery.of(context) or MediaQuery.maybeOf(context) '
+    problemMessage: 'Prefer using '
+        'MediaQuery.xxxOf(context) or MediaQuery.maybeXxxOf(context) '
+        'instead of MediaQuery.of(context) or MediaQuery.maybeOf(context) '
         'to avoid unnecessary rebuilds.',
   );
 
@@ -23,11 +51,11 @@ class UseMediaQueryXxxOf extends DartLintRule {
     CustomLintContext context,
   ) {
     context.registry.addMethodInvocation((node) {
-      // validate method name
+      // Do nothing if the method name is not `of` and not `maybeOf`.
       final methodName = node.methodName;
       if (methodName.name != 'of' && methodName.name != 'maybeOf') return;
 
-      // validate method argument
+      // Do nothing if the method argument's type is not `BuildContext`.
       final arguments = node.argumentList.arguments;
       if (arguments.length > 1) return;
       final firstArgument = node.argumentList.arguments.firstOrNull?.staticType;
@@ -38,7 +66,7 @@ class UseMediaQueryXxxOf extends DartLintRule {
       ).isExactlyType(firstArgument);
       if (!hasContextParam) return;
 
-      // validate library
+      // Do nothing if the package of method is not `flutter`.
       final library = methodName.staticElement?.library;
       if (library == null) return;
       final libraryUri = Uri.tryParse(library.identifier);
@@ -46,11 +74,11 @@ class UseMediaQueryXxxOf extends DartLintRule {
       if (libraryUri.scheme != 'package' ||
           libraryUri.pathSegments.first != 'flutter') return;
 
-      // validate operator
+      // Do nothing if the operator of method is not `.`.
       final operatorToken = node.operator;
       if (operatorToken?.type != TokenType.PERIOD) return;
 
-      // validate class name
+      // Do notion if the class of method is not `MediaQuery`.
       final target = node.realTarget;
       if (target is! SimpleIdentifier) return;
       final isMediaQuery = target.name == 'MediaQuery';
@@ -67,16 +95,18 @@ class UseMediaQueryXxxOf extends DartLintRule {
 }
 
 class _ReplaceWithMediaQueryXxxOf extends DartFix {
-  // fixed value for range of "of(context)."
+  /// Fixed value for range of `of(context).` statement.
   static const int _rangeDeltaForOf = 12;
 
-  // fixed value for range of "of(context)?."
+  /// Fixed value for range of `of(context)?.` statement.
   static const int _rangeDeltaForOfWithQuestionPeriod = 13;
 
-  // fixed value for range of "maybeOf(context)?."
+  /// Fixed value for range of `maybeOf(context)?.` statement.
   static const int _rangeDeltaForMaybeOf = 18;
 
-  // https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/widgets/media_query.dart#L36C4-L36C4
+  /// All data that `MediaQueryData` depends on.
+  ///
+  /// See also: https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/widgets/media_query.dart#L36C4-L36C4
   static const List<String> _properties = [
     'size',
     'orientation',
@@ -103,7 +133,7 @@ class _ReplaceWithMediaQueryXxxOf extends DartFix {
     required PropertyAccess node,
     required String property,
   }) {
-    // validate operator
+    // Do nothing if the operator of method is not `.` and not `?.`.
     final operatorToken = node.operator;
     if (operatorToken.type != TokenType.PERIOD &&
         operatorToken.type != TokenType.QUESTION_PERIOD) return;
@@ -111,7 +141,8 @@ class _ReplaceWithMediaQueryXxxOf extends DartFix {
     final methodTarget = node.realTarget;
     if (methodTarget is! MethodInvocation) return;
     final methodName = methodTarget.methodName.name;
-
+    // Do nothing if the method name which the property depends on
+    // is not `of` and not `maybeOf`.
     if (methodName == 'of') {
       final isQuestionPeriod = operatorToken.type == TokenType.QUESTION_PERIOD;
       final delta = isQuestionPeriod
@@ -163,6 +194,7 @@ class _ReplaceWithMediaQueryXxxOf extends DartFix {
     context.registry.addPropertyAccess((node) {
       if (!node.sourceRange.intersects(analysisError.sourceRange)) return;
 
+      // Do nothing if MediaQueryData doesn't have the property.
       final property = node.propertyName.name;
       if (!_properties.contains(property)) return;
 
