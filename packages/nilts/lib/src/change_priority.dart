9836fa03280ed85cccb9a37c55e77b598005a77c
ronnnnn <xxxx.xyzs.xxxx@gmail.com>
2023-10-29 14:59:52 +0900

    docs: notes for ignore fix priority

--- packages/nilts/lib/src/change_priority.dart
+++ packages/nilts/lib/src/change_priority.dart
@@ -1,6 +1,12 @@
 // ignore_for_file: comment_references
 
 /// A class for defining priorities of quick fix and assist in nilts.
+///
+/// Fixes to ignore rules are defined on [IgnoreCode].
+/// These priorities are 34 and 35.
+///
+/// See also:
+///   - [IgnoreCode](https://github.com/invertase/dart_custom_lint/blob/1df2851a80ccdc5a2bda4418006560f49c03b8ec/packages/custom_lint_builder/lib/src/ignore.dart#L102)
 class ChangePriority {
   /// The priority for [_AddTextScaleFactor].
   static const int addTextScaleFactor = 100;
