c3467c37f69204f153c37c669af6935e620f8344
Seiya Kokushi <xxxx.xyzs.xxxx@gmail.com>
2024-02-24 11:57:49 +0900

    fix: ignore defined_value_getter_type if return Future (#100)
    fix: ignore defined_value_getter_type when return furure

--- packages/nilts_test/test/lints/defined_value_getter_type.dart
+++ packages/nilts_test/test/lints/defined_value_getter_type.dart
@@ -11,30 +11,36 @@ class MainButton extends StatelessWidget {
   const MainButton(
     void Function() this.onPressed,
     // expect_lint: defined_value_getter_type
-    int Function() this.onReturnPressed, {
+    int Function() this.onReturnPressed,
+    Future<int> Function() this.onFutureReturnPressed, {
     void Function()? this.onNullablePressed,
     void Function(int)? this.onParamPressed,
     // expect_lint: defined_value_getter_type
     int Function()? this.onNullableReturnPressed,
+    Future<int> Function()? this.onNullableFutureReturnPressed,
     super.key,
   });
 
   final void Function() onPressed;
   // expect_lint: defined_value_getter_type
   final int Function() onReturnPressed;
+  final Future<int> Function() onFutureReturnPressed;
   final void Function()? onNullablePressed;
   final void Function(int)? onParamPressed;
   // expect_lint: defined_value_getter_type
   final int Function()? onNullableReturnPressed;
+  final Future<int> Function()? onNullableFutureReturnPressed;
 
   void _onPressed(
     void Function() onPressed,
     // expect_lint: defined_value_getter_type
-    int Function() onReturnPressed, {
+    int Function() onReturnPressed,
+    Future<int> Function() onFutureReturnPressed, {
     void Function()? onNullablePressed,
     void Function(int)? onParamPressed,
     // expect_lint: defined_value_getter_type
     int Function()? onNullableReturnPressed,
+    Future<int> Function()? onNullableFutureReturnPressed,
   }) {}
 
   @override
@@ -44,6 +50,7 @@ class MainButton extends StatelessWidget {
         _onPressed(
           () {},
           () => 0,
+          () async => 0,
         );
         onPressed();
       },
@@ -55,17 +62,21 @@ class MainButton extends StatelessWidget {
 final void Function() globalFunction = () {};
 // expect_lint: defined_value_getter_type
 final int Function() globalReturnFunction = () => 0;
+final Future<int> Function() globalFutureReturnFunction = () async => 0;
 const void Function()? globalNullableFunction = null;
 const void Function(int)? globalParamFunction = null;
 // expect_lint: defined_value_getter_type
 const int Function()? globalNullableReturnFunction = null;
+const Future<int> Function()? globalNullableFutureReturnFunction = null;
 
 void _globalFunction(
   void Function() onPressed,
   // expect_lint: defined_value_getter_type
-  int Function() onReturnPressed, {
+  int Function() onReturnPressed,
+  Future<int> Function() onFutureReturnPressed, {
   void Function()? onNullablePressed,
   void Function(int)? onParamPressed,
   // expect_lint: defined_value_getter_type
   int Function()? onNullableReturnPressed,
+  Future<int> Function()? onNullableFutureReturnPressed,
 }) {}
